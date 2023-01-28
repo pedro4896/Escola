4efd2fbcb7d3d8a3261fdaa16a3114c59e423f95
Pedro Victor <pedrovictordasilvapereira4896@gmail.com>
2023-01-28 19:01:40 -0300

    Add files via upload

---
+++ escola/php/frequência/2Ano/frequencia_ciencias.php
@@ -0,0 +1,80 @@
+<?php
+    include('../../verifica_login.php');
+    include('../../conexaoBd.php');
+
+    $data = $_POST['data'];
+    $pauta = $_POST['pauta'];
+
+    $docente = $banco->query("SELECT * FROM usuario 
+    INNER JOIN docente ON usuario.cpf = docente.cpf_usuario 
+    INNER JOIN leciona ON usuario.cpf = leciona.cpf_usuario 
+    INNER JOIN ensina ON usuario.cpf = ensina.cpf_usuario
+    WHERE ensina.codigo_disciplinas = '2_CIEN' AND leciona.turma_serie = '2 Ano'");
+    $cont = $banco->query("SELECT COUNT(matricula) AS quantidade FROM discentes WHERE turma_serie = '2 Ano';");
+    $discente = $banco->query("SELECT * FROM discentes WHERE turma_serie = '2 Ano' AND situacao = 'Deferido' ORDER BY nome ASC;");
+    $falta = array();
+    $presenca = array();
+    $justificativa = array();
+    if($cont){
+        foreach($cont as $linha){
+            $indice = $linha['quantidade'];
+        }
+    }
+
+    for($i = 1; $i<($indice + 1);$i++){
+        $post_falta="falta_$i";
+        $post_falta = $_POST[$post_falta];
+        if($post_falta == null){
+            $falta[$i] = 0;
+        }else{
+            $falta[$i] = $post_falta;
+        }
+    
+        $post_presenca="presenca_$i";
+        $post_presenca = $_POST[$post_presenca];
+        if($post_presenca == null){
+            $presenca[$i] = 0;
+        }else{
+            $presenca[$i] = $post_presenca;
+        }
+     
+        $post_justificativa ="justificativa_$i";
+        $post_justificativa = $_POST[$post_justificativa];
+        if($post_justificativa == null){
+            $justificativa[$i] = "-";
+        }else{
+            $justificativa[$i] = $post_justificativa;
+        }       
+    }
+
+    if($docente){
+        foreach($docente as $linha){
+            $cpf = $linha['cpf'];
+        }
+    }
+
+    $i = 1;
+    $matricula = array();
+    if($discente){
+        foreach($discente as $linha){
+           $matricula[$i] = $linha['matricula'];
+           $i++;
+        }
+    }
+
+    for($x = 1; $x<($indice + 1);$x++){
+        $frequencia = $banco->query("INSERT INTO `frequencia` (`data_frequencia`, `faltas`, `presenca`, `justificativa`, `codigo_disciplinas`, `matricula_discente`, `cpf_usuario`, `turma_serie`)
+                                     VALUES ('$data', $falta[$x], $presenca[$x], '$justificativa[$x]', '2_CIEN', '$matricula[$x]', '$cpf', '2 Ano');");
+        if($frequencia){}
+    }   
+    
+    $aula = $banco->query("INSERT INTO `aula` (`data_aula`, `pauta`, `codigo_disciplinas`, `cpf_usuario`, `turma_serie`)
+                           VALUES ('$data', '$pauta', '2_CIEN', '$cpf', '2 Ano')");
+    
+    if($aula){
+        echo("<script>window.location.href='ciencias.php';</script>");
+    }else{
+        print_r($banco->errorInfo());
+    }
+    
+?>
---
+++ escola/php/frequência/3Ano/ingles.php
@@ -0,0 +1,72 @@
+<?php
+    include('../../verifica_login.php');
+    include('../../conexaoBd.php');
+?>
+<!DOCTYPE html>
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width", initial-scale="1">
+                    <title>Frequ&ecirc;ncia 3Ano - Ingl&ecirc;s</title>
+                    <link rel="stylesheet" type="text/css" href="../frequencia_disciplinas.css">
+                </head>
+                <body> 
+                    <div id="sair">
+                        <a href= "../../logout.php" id="encerrar"><img src="../../../imagens/encerrar.png" alt= "Encerrar Sessão"/><br>Encerrar<br>Sess&atilde;o</a>
+                    </div>
+                    
+                    <div><table>
+                        <form action="frequencia_ingles.php" method="POST" onsubmit=" return validarTudo()">
+                            <tr><th colspan=5>Frequ&ecirc;ncia 3 Ano - Ingl&ecirc;s</th></tr>
+                            <tr id=titulo>
+                                <?php
+                                    $result = $banco->query("SELECT * FROM usuario 
+                                    INNER JOIN docente ON usuario.cpf = docente.cpf_usuario 
+                                    INNER JOIN leciona ON usuario.cpf = leciona.cpf_usuario 
+                                    INNER JOIN ensina ON usuario.cpf = ensina.cpf_usuario
+                                    WHERE ensina.codigo_disciplinas = '3_ING' AND leciona.turma_serie = '3 Ano'");
+                                    if($result){
+                                        foreach($result as $linha){
+                                            $nome = $linha['nome'];
+                                            $serie = $linha['turma_serie'];
+                                        }
+                                    }
+                                    echo("<td>Docente:".@$nome."</td><td>Disciplina: Ingl&ecirc;s</td><td>S&eacute;rie: ".@$serie."</td><td colspan=2>Data: <input type=date name=data  id=data required></td>");
+                                ?>
+                            </tr>
+                            <tr><td>Matr&iacute;cula</td><td>Nome</td><td>Presen&ccedil;a</td><td>Faltas</td><td>Justificativa</td><tr>
+                            <tbody id=tbody>
+                                <?php
+                                    $result = $banco->query("SELECT * FROM discentes WHERE turma_serie = '3 Ano' AND situacao = 'Deferido' ORDER BY nome ASC;");
+                                    if($result){
+                                        $cont = 1;
+                                        foreach($result as $linha){
+                                            echo("<tr>");
+                                                echo("<td>".$linha['matricula']."</td>"."<td>".$linha['nome']."</td>
+                                                      <td><input type=text name=presenca_$cont  id=presenca></td><td><input type=text name=falta_$cont  id=falta>
+                                                      </td><td><textarea name=justificativa_$cont  id=justificativa rows=3 cols=25></textarea></td>
+                                                ");
+                                            echo("</tr>");
+                                         $cont++;   
+                                        }
+                                    } 
+                                ?>
+                                <tr>
+                                    <td colspan=5>  
+                                        <div><span>Observa&ccedil;&otilde;es sobre a aula<span></div>
+                                        <textarea name="pauta" rows="20" cols="50" required></textarea>
+                                    </td>    
+                                </tr>
+                                <tr>
+                                    <td colspan=5>
+
+                                        <button type="submit">Enviar</button>
+                                    </td>
+                                </tr>      
+                            </tbody>    
+                        </form>
+                    </div>    
+                </body>        
+            </html>                
+
+                            
